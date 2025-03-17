d538b5e40ea7b7277d587da27635b3ff3f6cd927
Ankit Sharma <ankitsh474@gmail.com>
2025-03-17 22:04:58 +0530

    add many to many relation

--- src/entities/property.entity.ts
+++ src/entities/property.entity.ts
@@ -1,17 +1,44 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
-
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { PropertyFeature } from './propertyFeature.entity';
+import { User } from './user.entity';
 
 @Entity()
-export class Property{
-    @PrimaryGeneratedColumn()
-    id:number
+export class Property {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name:string
+  @Column()
+  name: string;
 
-    @Column()
-    description:string;
+  @Column()
+  description: string;
 
-    @Column({default:0})
-    price:number
+  @Column({ default: 0 })
+  price: number;
+
+  @OneToOne(
+    () => PropertyFeature,
+    (propertyFeature) => propertyFeature.property,
+    {cascade:true}
+    // {cascade:["update"]}
+  )
+//   @JoinColumn()
+  propertyFeature: PropertyFeature;
+
+  @ManyToOne(()=>User,(user)=>user.properties)
+  @JoinColumn({name:"ownerId"})
+   user:User;
+
+   @ManyToMany(()=>User,(user)=>user.likedProperties)
+   likedBy:User[]
+
 }
